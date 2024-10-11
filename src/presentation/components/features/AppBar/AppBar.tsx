bc7b3a9b5bb7c9380632b0ed9f1aada30ecb5a32
Natalia Bezerra <b1z3rr4@gmail.com>
2024-10-11 01:00:35 -0300

    feat: logout

--- src/presentation/components/features/AppBar/AppBar.tsx
+++ src/presentation/components/features/AppBar/AppBar.tsx
@@ -1,29 +1,49 @@
 import { getInitials } from "@/application/utils/getInitials";
 import { useAuth } from "@/presentation/contexts/AuthContext";
+import { useState } from "react";
 import { Paragraph } from "../../elements/Paragraph";
+import { DropDown } from "../../modules/DropDown";
 import { Logo } from "../../modules/Logo";
 import * as S from "./styles";
 
 export function AppBar() {
-  const { currentUser } = useAuth();
+  const { currentUser, logout } = useAuth();
 
-  console.log(currentUser);
+  if (!currentUser) return;
 
   const displayName = currentUser?.email?.split("@")[0] as string;
 
   const initials = getInitials(displayName);
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleItemClick = () => {
+    setIsOpen(false);
+    logout();
+  };
+
   return (
     <S.AppBarContainer>
       <Logo size="sm" />
-      <S.MenuContainer>
-        <Paragraph>{displayName}</Paragraph>
-        <S.ProfileContainer>
-          <S.ProfileImg src={currentUser?.photoURL || ""}>
-            {!currentUser?.photoURL && initials}
-          </S.ProfileImg>
-        </S.ProfileContainer>
-      </S.MenuContainer>
+      <DropDown.Root>
+        <DropDown.Trigger onClick={toggleDropdown}>
+          <S.MenuContainer>
+            <Paragraph>{displayName}</Paragraph>
+            <S.ProfileContainer>
+              <S.ProfileImg src={currentUser?.photoURL || ""}>
+                {!currentUser?.photoURL && initials}
+              </S.ProfileImg>
+            </S.ProfileContainer>
+          </S.MenuContainer>
+        </DropDown.Trigger>
+        <DropDown.Menu isOpen={isOpen}>
+          <DropDown.Item onClick={handleItemClick}>Sair</DropDown.Item>
+        </DropDown.Menu>
+      </DropDown.Root>
     </S.AppBarContainer>
   );
 }
