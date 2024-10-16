d2394408208cbf9fc4680451c86993e2e2c68cf3
Natalia Bezerra <b1z3rr4@gmail.com>
2024-10-16 08:26:07 -0300

    fix: ajuste logo

--- src/presentation/components/modules/Logo/styles.ts
+++ src/presentation/components/modules/Logo/styles.ts
@@ -3,8 +3,8 @@ import { Paragraph } from "@/presentation/components/elements/Paragraph";
 
 export const AppLogo = styling(Paragraph) <{ fontSize: number }>`
     padding: 12px;
-    font-size: ${({ fontSize }) => fontSize}px;
     font-weight: 600;
+    font-size: ${({ fontSize }) => fontSize}px;
     color: ${({ theme }) => theme.primaryColor};
     font-family: ${({ theme }) => theme.poppinsFont};
 `;
